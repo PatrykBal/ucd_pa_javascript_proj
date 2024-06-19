aa3cdb39a347335a1601511e552a709dc96b2b07
PatrykBal <patrykbalecki@gmail.com>
2024-06-19 17:10:17 +0200

    finished form js in script.js

--- script.js
+++ script.js
@@ -22,24 +22,32 @@ excerciseContainers.forEach((item, i) => {
 
 const url = 'https://exercisedb.p.rapidapi.com/exercises?limit=40&offset=0';
 const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'b7158ec930mshde39d189ea9e94fp177b2fjsn5149093c923e',
-		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'b7158ec930mshde39d189ea9e94fp177b2fjsn5149093c923e',
+        'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+    }
 };
 
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+});
 
-async function fetchData(){
+document.getElementById('search-btn').addEventListener('click', () => {
+    const searchQuery = document.getElementById('search-input').value.toLowerCase();
+    fetchData(searchQuery);
+});
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-    displayExercises(results);
-
-} catch (error) {
-	console.error(error);
+async function fetchData(searchQuery = '') {
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const filteredExercises = result.filter(exercise =>
+            exercise.name.toLowerCase().includes(searchQuery)
+        );
+        displayExercises(filteredExercises);
+    } catch (error) {
+        console.error(error);
     }
 }
 
@@ -77,26 +85,60 @@ function displayExercises(exercises) {
     });
 }
 
-document.addEventListener('DOMContentLoaded', fetchData);
-
-document.getElementById('search-btn').addEventListener('click', () => {
-    const searchQuery = document.getElementById('search-input').value.toLowerCase();
-    fetchData(searchQuery);
-});
-
-async function fetchData(searchQuery = '') {
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        const filteredExercises = result.filter(exercise =>
-            exercise.name.toLowerCase().includes(searchQuery)
-        );
-        displayExercises(filteredExercises);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
 /* FORM */
 
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form-message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form-message-success", "form-message-error");
+    messageElement.classList.add(`form-message--${type}`);
+}
+
+function setInputError (inputElement, message) {
+    inputElement.classList.add("form-input-error");
+    inputElement.parentElement.querySelector(".form-input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form-input-error");
+    inputElement.parentElement.querySelector(".form-input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form-hidden");
+        createAccountForm.classList.remove("form-hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form-hidden");
+        createAccountForm.classList.add("form-hidden");
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // Perform your AJAX/Fetch login
+
+        setFormMessage(loginForm, "error", " Invalid username/password combination");
+    });
+
+    document.querySelectorAll(".form-input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
 
