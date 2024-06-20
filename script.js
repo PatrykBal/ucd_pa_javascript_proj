9bae4fbfe2f99a0c957ab8f66f84ae0290a25e87
PatrykBal <patrykbalecki@gmail.com>
2024-06-20 17:30:59 +0200

    added css and js animation to hero section and working on media queries

--- script.js
+++ script.js
@@ -17,6 +17,22 @@ excerciseContainers.forEach((item, i) => {
     })           
 })
 
+/* HERO SECTION ANIMATION */
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 /* FORM */
 
 function setFormMessage(formElement, type, message) {
